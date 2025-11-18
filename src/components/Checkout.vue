5f7f209a13d447b4c787329c3379718833bb42e3
Zeel2103 <yagnik.zeel@gmail.com>
2025-11-18 17:19:14 +0000

    Updated build files for github pages

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -6,7 +6,11 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-
+<p>Name: </p>
+<p>Email: </p>
+<p>Phone Number: </p>
+<p>Order Total: </p>
+<button>Checkout</button>
 
     
 </template>
